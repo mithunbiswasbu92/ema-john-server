17171d6d027b96fd7bd397487903e778a7ae449f
Mithun Biswas <mithunbiswasbu92@gmail.com>
2021-08-09 00:41:23 +0600

    ema-john-server code

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wizys.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productsCollection = client.db("emaJohnStore").collection("products");
+    const ordersCollection = client.db("emaJohnStore").collection("orders");
+
+    app.post('/addProduct', (req, res) => {
+        const products = req.body; 
+        productsCollection.insertOne(products)
+        .then(result => {
+            console.log(result.insertedCount);
+            res.send(result.insertedCount);
+        })
+    })
+    app.get('/products', (req, res) => {
+        productsCollection.find({})
+        .toArray((err, document) => {
+            res.send(document);
+        })
+    })
+    app.get('/product/:key', (req, res) => {
+        productsCollection.find({key: req.params.key})
+        .toArray((err, document) => {
+            res.send(document[0]);
+        })
+    })
+
+    app.post('/productsByKeys', (req, res) => {
+        const productKeys = req.body;
+        productsCollection.find({key: {$in: productKeys}})
+        .toArray((err, document) => {
+            res.send(document)
+        })
+    })
+
+    app.post('/addOrder', (req, res) => {
+        const order = req.body; 
+        ordersCollection.insertOne(order)
+        .then(result => { 
+            console.log(result)
+            res.send(result.insertedCount > 0);
+        })
+    })
+});
+
+app.listen(process.env.PORT || port)
